c959efd9e9b3effc946ec6eefb5502bd9523d270
nachoofr <sachhyamnyachhyon@gmail.com>
2025-11-26 13:44:16 +0545

    resolved error and added database configuration

---
+++ src/main/java/com/intern/digitallendingsystem/DigitalLendingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.intern.digitallendingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalLendingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalLendingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/intern/digitallendingsystem/DigitalLendingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intern.digitallendingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalLendingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
